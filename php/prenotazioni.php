040e0902e2a61561f48708b58ee8e0d99ef7767f
dario platania <dariopl91@gmail.com>
2016-05-31 14:22:09 +0200

    insert prenotazioni.php file e modificata la index.html

---
+++ php/prenotazioni.php
@@ -0,0 +1,58 @@
+<html>
+
+<title>Prenotazioni</title>
+
+<head>
+  <head>
+
+
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  </head>
+</head>
+
+<body>  
+
+  <table class="table">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Materia</th>
+      <th>Codice</th>
+      <th>Voto</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>BDMS</td>
+      <td>0123</td>
+      <td>27</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>OMA</td>
+      <td>7894</td>
+      <td>27</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>ASE</td>
+      <td>5234</td>
+      <td>24</td>
+    </tr>
+  </tbody>
+</table>
+
+<footer>
+ <div class="container">
+   <p class="text-muted">Realizzato da: Dario Platania</p>
+   <p>Email:&nbsp;<a href="mailto:dariopl91@gmail.com">dariopl91@gmail.com</a></p>
+ </div>
+</footer>
+
+</body>
+</html>
--- php/welcome.php
+++ php/welcome.php
@@ -21,7 +21,7 @@
          <div class="navbar-header">
            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
              <span class="sr-only">Toggle navigation</span>
-             <span class="icon-bar"></span>
+             <span class="icon-bar">Prenotazioni</span>
              <span class="icon-bar"></span>
              <span class="icon-bar"></span>
            </button>
