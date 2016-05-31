64eeb37071547a8a8e018699620539504414454f
dario platania <dariopl91@gmail.com>
2016-05-31 12:37:50 +0200

    insert signup function (incomplete)

---
+++ php/signup.php
@@ -0,0 +1,82 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'print3D');
+define('DB_USER','root');
+define('DB_PASSWORD','california');
+
+$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " .     mysql_error());
+$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+
+
+
+$username = $_POST['username'];
+$password =  $_POST['password'];
+$email =  $_POST['email'];
+$query = "INSERT INTO users (username,password,email) VALUES ('$username','$password','$email')";
+$data = mysql_query ($query)or die(mysql_error());
+
+?>
+
+<html>
+<head>
+
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+  <title>Print3d</title>
+</head>
+<body>
+  <div class="container">
+        <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <div class="panel panel-login">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-6">
+                  <a href="signin.php" id="register-form-link">Vai al Log In</a>
+                </div>
+                <div class="col-xs-6 col-md-4">
+                  <a href="../index.html" id="register-form-link">Home</a>
+                </div>
+              </div>
+              <hr>
+              <div class="row">
+                <div class="col-xs-8">
+                  <a href="#" class="active" id="login-form-link">Registrati</a>
+                </div>
+              </div>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-lg-12">
+                  <form id="login-form" action="" method="post" role="form" style="display: block;">
+                    <div class="form-group">
+                      <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <input type="email" name="email" id="email" tabindex="3" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                      <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3">
+                          <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Iscriviti">
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
--- php/welcome.php
+++ php/welcome.php
@@ -1,15 +1,50 @@
 <?php
    include('session.php');
+   $user = $_SESSION['login_user'];
 ?>
 <html>
 
    <head>
-      <title>Welcome </title>
+     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+     <!-- jQuery library -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+     <!-- Latest compiled JavaScript -->
+     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+     <title>Print3d</title>
    </head>
 
    <body>
-      <h1>Welcome <?php echo $login_session; ?></h1>
-      <h2><a href = "logout.php">Sign Out</a></h2>
+     <nav class="navbar navbar-default">
+       <div class="container-fluid">
+         <!-- Brand and toggle get grouped for better mobile display -->
+         <div class="navbar-header">
+           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+             <span class="sr-only">Toggle navigation</span>
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+           </button>
+           <a class="navbar-brand" href="#">Print3D</a>
+         </div>
+
+         <!-- Collect the nav links, forms, and other content for toggling -->
+
+         <!-- Inizio Parte Login -->
+         <ul class="nav navbar-nav navbar-right">
+           <!--<li><a href="#">Link</a></li>-->
+           <li class="dropdown">
+             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Welcome <?php echo $user; ?>
+               <span class="caret"></span></a>
+             <ul class="dropdown-menu">
+               <li><a href = "logout.php">Sign Out</a></li>
+             </ul>
+           </li>
+         </ul>
+        <!-- Fine Parte Login -->
+       </div><!-- /.container-fluid -->
+     </nav>
+
    </body>
 
 </html>
