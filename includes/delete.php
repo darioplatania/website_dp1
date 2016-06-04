b134c6b5706eb3816a94c96e5d649ea3101ef809
dario platania <dariopl91@gmail.com>
2016-06-04 23:09:49 +0200

    insert redirect into delete.php

--- includes/delete.php
+++ includes/delete.php
@@ -1,10 +1,16 @@
 <?php
-mysql_connect("localhost","root","california");
-mysql_select_db("print3D");
-if(isset($_GET['delete_id']))
+if(!$_SESSION['email'])
 {
- $sql_query="DELETE FROM prenotazioni WHERE id=".$_GET['delete_id'];
- mysql_query($sql_query);
- header("Location: ../php/prenotazioni.php");
+    header('Location: ../index.php');
+}
+else{
+ mysql_connect("localhost","root","california");
+ mysql_select_db("print3D");
+    if(isset($_GET['delete_id']))
+    {
+     $sql_query="DELETE FROM prenotazioni WHERE id=".$_GET['delete_id'];
+     mysql_query($sql_query);
+     header("Location: ../php/prenotazioni.php");
+     }
 }
 ?>
